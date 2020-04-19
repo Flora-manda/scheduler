b88f67d54209e06bfd1d1a2e988b43a4c2ff98f9
Flora Uwadiegwu <flora@Floras-MacBook-Pro.local>
2020-04-18 20:04:37 -0400

    update the application with spot remaining functionality

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -33,41 +33,74 @@ export function useApplicationData() {
     });
   }, []);
 
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
+  function findSpots(appointmentId, type) {
+    //Loop through the appointment key and find the matching id
 
+    let dayFound = {};
+
+    for (let item of state.days) {
+      if (item.appointments.includes(appointmentId)) {
+        dayFound = item;
+      }
+    }
+    //Find the index of each day in the days object
+    //Once found update the spots count (Book removes a spot, Delete adds a spot)
+    let dayIndex = dayFound.id - 1;
+
+    let spotResult = dayFound.spots;
+    if (type === "Book") {
+      spotResult -= 1;
+    }
+
+    if (type === "Delete") {
+      spotResult += 1;
+    }
+
+    //Update the spots value with the new spot count
+    let newDaysArray = state.days;
+    newDaysArray[dayIndex].spots = spotResult;
+
+    //SetState with the new days information
+    setState({ ...state, days: newDaysArray });
+  }
+
+  function bookInterview(id, interview) {
     const putRequest = axios
       .put(`/api/appointments/${id}`, {
         interview,
       })
       .then(() => {
+        const appointment = {
+          ...state.appointments[id],
+          interview: { ...interview },
+        };
+        const appointments = {
+          ...state.appointments,
+          [id]: appointment,
+        };
         setState({ ...state, appointments });
-      });
+      })
+      .then(findSpots(id, "Book"));
 
     return putRequest;
   }
 
-  function deleteInterview(id, interview) {
+  function deleteInterview(id) {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview },
+      interview: null,
     };
     const appointments = {
       ...state.appointments,
       [id]: appointment,
     };
 
-    const deleteRequest = axios.delete(`/api/appointments/${id}`).then(() => {
-      setState({ ...state, appointments });
-    });
+    const deleteRequest = axios
+      .delete(`/api/appointments/${id}`)
+      .then(() => {
+        setState({ ...state, appointments });
+      })
+      .then(findSpots(id, "Delete"));
 
     return deleteRequest;
   }
